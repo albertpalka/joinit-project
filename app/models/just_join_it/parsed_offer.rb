87d65a0555b9f91e11c99c2861be761cb2f12d6c
albertspark <albert.palka@sparksolutions.co>
2020-02-22 23:29:13 +0100

    Fixed failing app

--- app/models/just_join_it/parsed_offer.rb
+++ app/models/just_join_it/parsed_offer.rb
@@ -4,6 +4,9 @@ module JustJoinIt
   class ParsedOffer < ApplicationRecord
     belongs_to :raw_datum
 
+    include PgSearch::Model	    validates :body, presence: true
+    pg_search_scope :search_full_text, against: %i[skills city]
+
     validates :body, presence: true
   end
 end
