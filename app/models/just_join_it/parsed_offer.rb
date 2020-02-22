40a7874d2012d0dea38db1b78ed5b126b50ec905
albertspark <albert.palka@sparksolutions.co>
2020-02-22 23:32:36 +0100

    Fixed copy-pasted version

--- app/models/just_join_it/parsed_offer.rb
+++ app/models/just_join_it/parsed_offer.rb
@@ -4,7 +4,7 @@ module JustJoinIt
   class ParsedOffer < ApplicationRecord
     belongs_to :raw_datum
 
-    include PgSearch::Model	    validates :body, presence: true
+    include PgSearch::Model
     pg_search_scope :search_full_text, against: %i[skills city]
 
     validates :body, presence: true
