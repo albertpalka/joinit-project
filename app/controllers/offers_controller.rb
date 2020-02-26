fe0de0fe20498a497e51a9c7fc4b95133fc7f737
albertspark <albert.palka@sparksolutions.co>
2020-02-26 21:23:29 +0100

    more fixes

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,37 +2,37 @@ class OffersController < ApplicationController
   include Pagy::Backend
 
   def index
-    if params[:skills]
-      @pagy, @offer = pagy(JustJoinIt::RawDatum.first
-                                               .parsed_offers
-                                               .search_full_text(params[:skills]), items: 50)
+  #   if params[:skills]
+  #     @pagy, @offer = pagy(JustJoinIt::RawDatum.first
+  #                                              .parsed_offers
+  #                                              .search_full_text(params[:skills]), items: 50)
 
-    elsif params[:city]
-      @pagy, @offer = pagy(JustJoinIt::RawDatum.first
-                                                .parsed_offers
-                                                .search_full_text(params[:city]), items: 50)
-    elsif params[:skills] && params[:city]
-      @pagy, @offer = pagy(JustJoinIt::RawDatum.first
-                                               .parsed_offers
-                                               .search_full_text(params[:skills, :city]), items: 50)
-    else
-      @pagy, @offer = pagy(JustJoinIt::RawDatum.first.parsed_offers)
-    end
-  end
+  #   elsif params[:city]
+  #     @pagy, @offer = pagy(JustJoinIt::RawDatum.first
+  #                                               .parsed_offers
+  #                                               .search_full_text(params[:city]), items: 50)
+  #   elsif params[:skills] && params[:city]
+  #     @pagy, @offer = pagy(JustJoinIt::RawDatum.first
+  #                                              .parsed_offers
+  #                                              .search_full_text(params[:skills, :city]), items: 50)
+  #   else
+  #     @pagy, @offer = pagy(JustJoinIt::RawDatum.first.parsed_offers)
+  #   end
+  # end
 
-  def skills
-    @skills = JustJoinIt::ParsedOffer.all
-                                     .pluck(:skills)
-                                     .map { |s| s.split(',') }
-                                     .flatten
-                                     .collect(&:squish)
-                                     .uniq
-                                     .sort
+  # def skills
+  #   @skills = JustJoinIt::ParsedOffer.all
+  #                                    .pluck(:skills)
+  #                                    .map { |s| s.split(',') }
+  #                                    .flatten
+  #                                    .collect(&:squish)
+  #                                    .uniq
+  #                                    .sort
   end
 
   private
 
-  def offer_params
-    params.require(:parsed_offers).permit(:skills, :city)
-  end
+  # def offer_params
+  #   params.require(:parsed_offers).permit(:skills, :city)
+  # end
 end
