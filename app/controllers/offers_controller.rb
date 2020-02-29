306af7dd0af951147cd78ba5b3c2689a174e4bbf
albertspark <albert.palka@sparksolutions.co>
2020-02-29 21:36:26 +0100

    fixed search with multiple params

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -6,6 +6,16 @@ class OffersController < ApplicationController
                      .order('RANDOM()')
                      .search_full_text(params[:title])
                      .paginate(page: params[:page], per_page: 40)
+    elsif params[:city]
+      @offers = Offer.with_salary
+                     .order('RANDOM()')
+                     .search_full_text(params[:city])
+                     .paginate(page: params[:page], per_page: 40)
+    elsif params[:title] && params[:city]
+      @offers = Offer.with_salary
+                     .order('RANDOM()')
+                     .search_full_text(params[:title, :city])
+                     .paginate(page: params[:page], per_page: 40)
     else
       @offers = Offer.with_salary
                      .order('RANDOM()')
@@ -16,6 +26,6 @@ class OffersController < ApplicationController
   private
 
   def offer_params
-    params.require(:offers).permit(:title)
+    params.require(:offers).permit(:title, :city)
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   scope :with_salary, -> { where.not(salary: '') }
 
   include PgSearch::Model
-  pg_search_scope :search_full_text, against: %i[title]
+  pg_search_scope :search_full_text, against: %i[title city]
 end
